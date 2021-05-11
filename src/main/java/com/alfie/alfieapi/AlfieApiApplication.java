9325566121b303ea8a9ea21679350e805da9f00a
joaovicdsantos <joaovicdsantos@gmail.com>
2021-05-11 13:29:49 -0300

    feat: Initial commit :tada:

---
+++ src/main/java/com/alfie/alfieapi/AlfieApiApplication.java
@@ -0,0 +1,13 @@
+package com.alfie.alfieapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfieApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlfieApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alfie/alfieapi/AlfieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alfie.alfieapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfieApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
